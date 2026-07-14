cb6f5257db963d3abc5c1c81f08a8ed14133487f
munkur <munkur@autocapcut.live>
2026-07-14 04:33:59 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "@/components/landing/Navbar";
 import HeroSection from "@/components/landing/HeroSection";
-import SectionDivider from "@/components/landing/SectionDivider";
 import CategoryGallery from "@/components/landing/CategoryGallery";
 import FeaturedProducts from "@/components/landing/FeaturedProducts";
 import WhyChooseUs from "@/components/landing/WhyChooseUs";
@@ -16,15 +15,10 @@ export default function Home() {
     <div className="min-h-screen bg-beige">
       <Navbar />
       <HeroSection />
-      <SectionDivider />
       <CategoryGallery />
-      <SectionDivider />
       <FeaturedProducts />
-      <SectionDivider />
       <WhyChooseUs />
-      <SectionDivider />
       <AboutSection />
-      <SectionDivider />
       <Testimonials />
       <StoreLocation />
       <ContactForm />
